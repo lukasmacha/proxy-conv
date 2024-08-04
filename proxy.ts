4e79d5d00ab70ab5db78217412a7ab5a957758e6
Lukas Machacek <lukasmachacek@zoneandco.com>
2024-08-04 13:53:37 +0200

    init version

---
+++ proxy.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+const PORT = 3000;
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+
+app.get('/fetch-data', async (req, res) => {
+  try {
+    const response = await axios.get('https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt');
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching data');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
